93a1ac9775ccbae9ce5452604d078a1a6df52da9
Dmitry <cavidan@cavidans-MacBook-Pro.local>
2024-07-28 17:07:19 +0400

    feat: Screenshot tests added

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,15 +2,16 @@ import { Route, Routes } from 'react-router-dom';
 import { LoginPage } from '../pages/LoginPage/LoginPage';
 import { RegisterPage } from '../pages/RegisterPage/RegisterPage';
 import { AppRoutes } from '@src/shared/config/routesPath';
+import { Suspense } from 'react';
 
 function App() {
   return (
-    <>
+    <Suspense fallback="Loading...">
       <Routes>
         <Route path={AppRoutes.login} element={<LoginPage />} />
         <Route path={AppRoutes.register} element={<RegisterPage />} />
       </Routes>
-    </>
+    </Suspense>
   );
 }
 
